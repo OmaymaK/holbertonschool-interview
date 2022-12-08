cbf7901a37affc1ad7c0c700cd1b6709bc9d0e81
OmaymaK <emmablink050@gmail.com>
2022-12-08 15:04:06 +0100

    lockboxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    Determines if all boxes can be opened.
+"""
+
+
+def canUnlockAll(boxes):
+    """
+        Return True if all boxes can be opened, else return False.
+    """
+
+    keys = [0]
+
+    for key in keys:
+        for idx in boxes[key]:
+            if idx not in keys and idx < len(boxes):
+                keys.append(idx)
+    for i in range(len(boxes)):
+        if i not in keys:
+            return False
+    return True
