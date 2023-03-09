d5f3a7a55e89bb36ea5a010e35bfea411ffb2a2f
OmaymaK <emmablink050@gmail.com>
2023-03-09 15:44:32 +0100

    utf 8 validation

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Checks if a given data set represents a valid UTF-8 encoding
+"""
+
+
+def validUTF8(data):
+
+    """
+    Return: True if data is a valid UTF-8 encoding, Else return False
+    """
+    try:
+        bytes(number & 0xFF for number in data).decode()
+        return True
+    except UnicodeDecodeError:
+        return False
