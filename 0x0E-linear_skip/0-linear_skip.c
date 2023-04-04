a54d9f34f37ea89a6bacd80329ed104ed632142a
OmaymaK <emmablink050@gmail.com>
2023-04-04 21:54:16 +0100

    skiplist interview project

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,50 @@
+#include "search.h"
+
+
+/**
+ * linear_skip - function that searches for a value in a sorted skip list.
+ * @list: pointer to the head of the list
+ * @value: value to search
+ * Return: pointer to the node containing the value or NULL
+ */
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+
+	skiplist_t *tmp = list;
+
+	if (list == NULL)
+		return (NULL);
+	while (tmp->express != NULL)
+	{
+		printf("Value checked at index [%lu] = [%d]\n",
+		 tmp->express->index, tmp->express->n);
+		if (tmp->express->n >= value)
+		{
+			printf("Value found between indexes [%lu] and [%ld]\n",
+			 tmp->index, tmp->express->index);
+			break;
+		}
+		tmp = tmp->express;
+	}
+	if (tmp->express == NULL)
+	{
+		for (list = tmp; list->next != NULL; list = list->next)
+		;
+		printf("Value found between indexes [%lu] and [%ld]\n",
+		 tmp->index, list->index);
+	}
+	for (list = tmp ; list != tmp->express ; list = list->next)
+	{
+		printf("Value checked at index [%lu] = [%d]\n",
+		 list->index, list->n);
+		if (value == list->n)
+		{
+			break;
+		}
+	}
+	if (list == tmp->express)
+		return (NULL);
+	else
+		return (list);
+}
