cb69dabe326868e10bf365b5b693a9986c86b482
LouayNouri <emmablink050@gmail.com>
2023-07-13 21:57:59 +0100

    task

---
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -0,0 +1,75 @@
+#include "list.h"
+
+/**
+ * add_node_end - Add a new node to the end of a double.
+ * circular linked list.
+ * @list: the list to modify.
+ * @str: the string to copy into the new node.
+ * Return: Address of the new node, or NULL on failure.
+ */
+
+List *add_node_end(List **list, char *str)
+{
+	List *new;
+	List *p;
+
+	if (!list || !str)
+		return (NULL);
+
+	new = malloc(sizeof(list));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	if (new->str == NULL)
+		return (NULL);
+	if (*list == NULL)
+	{
+		new->next = new->prev = new;
+		*list = new;
+		return (new);
+	}
+	p = (*list)->prev;
+	p ->next = new;
+	new->next = (*list);
+	new->prev = p;
+	(*list)->prev = new;
+	return (new);
+}
+
+/**
+ * add_node_begin - Add a new node to the begin of a double.
+ * circular linked list.
+ * @list: the list to modify.
+ * @str: the string to copy into the new node.
+ * Return: Address of the new node, or NULL on failure.
+ */
+
+List *add_node_begin(List **list, char *str)
+{
+	List *new;
+	List *p;
+
+	if (!list || !str)
+		return (NULL);
+
+	new = malloc(sizeof(list));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	if (new->str == NULL)
+		return (NULL);
+	if (*list == NULL)
+	{
+		new->next = new->prev = new;
+		*list = new;
+		return (new);
+	}
+	p = (*list)->prev;
+	new->next = (*list);
+	new->prev = p;
+	(*list)->prev = p->next = new;
+	*list = new;
+	return (new);
+}
