7c44705dae2463d5c967fda12abf92729b6a518f
LouayNouri <emmablink050@gmail.com>
2023-08-14 14:02:47 +0100

    Task

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,12 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <stdlib.h>
+
+int _putchar(char c);
+int _strlen(char *str);
+void _print_error(void);
+int _isDigit(char c);
+void _mul(char *nb, char *nb2, int nblength, int nb2length);
+
+#endif
