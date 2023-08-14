48519294d69c274c4ce5dbe8ec8e73030c9b9a6b
LouayNouri <emmablink050@gmail.com>
2023-08-14 13:47:48 +0100

    count

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,53 +1,34 @@
 #!/usr/bin/python3
-"""
-    Parses the title of all hot articles, and prints a sorted
-    count of given keywords (case-insensitive, delimited by spaces.
-"""
-import json
-import re
+"""Get hot posts from reddit sub."""
 import requests
-import time
 
 
-def count_elements(request, word_list, results):
-    """ Counts number of elements """
-    for title in request['data']['children']:
-        datas = title['data']['title'].split(" ")
-        for i in range(len(datas)):
-            datas[i] = datas[i].lower()
-            if (datas[i] in word_list):
-                if (datas[i] in results.keys()):
-                    results[datas[i]] += word_list.count(datas[i])
-                else:
-                    results[datas[i]] = word_list.count(datas[i])
-    return results
+def count_words(subreddit, word_list, after='', hot_list=None):
+    """Get posts of sub."""
+    word_list = [item.lower() for item in word_list]
+    if hot_list is None:
+        hot_list = {item: 0 for item in word_list}
 
+    if after is None:
+        for word in sorted(hot_list, key=lambda x: (-hot_list[x], x)):
+            if hot_list[word] != 0:
+                print("{}: {}".format(word, hot_list[word]))
+        return None
 
-def count_words(subreddit, word_list, results={}, param={'limit': 100}):
-    """ Main function to count and print the words """
-    baseLink = 'https://api.reddit.com/r/%s/hot.json' % subreddit
+    headers = {'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)"}
+    url = 'https://www.reddit.com'
+    res = requests.get('{}/r/{}/hot.json?after={}'
+                       .format(url, subreddit, after), headers=headers,
+                       allow_redirects=False)
 
-    if ('after' not in param):
-        word_list = [str.lower() for str in word_list]
+    if res.status_code == 200:
+        rj = res.json()
+        if rj.get('data') is None:
+            return None
+        for post in rj['data']['children']:
+            title = post['data']['title'].lower().split(' ')
+            for word in word_list:
+                hot_list[word] += title.count(word)
 
-    link = baseLink
-    customHeaders = {'User-agent': 'HolbertonSchoolTask'}
-    r = requests.get(link, headers=customHeaders,
-                     params=param, allow_redirects=False)
-
-    if (r.status_code != 200):
-        return
-    data = r.content
-
-    data = json.loads(data.decode('utf-8'))
-    param = {'limit': 100, 'count': 100, 'after': data['data'].get('after')}
-    if (data['data'].get('after') is None):
-        results = count_elements(data, word_list, results)
-        results = sorted(
-            results.items(), key=lambda x: (-x[1], x[0]), reverse=False
-        )
-        for i in results:
-            print("{}: {}".format(i[0], i[1]))
-    else:
-        results = count_elements(data, word_list, results)
-        count_words(subreddit, word_list, results, param)
+        return count_words(subreddit, word_list, rj['data']['after'], hot_list)
+    return None
